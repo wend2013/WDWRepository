4bcb5bf55f3f061b0bcbda6fe684245a3f120809
wendongwei <wend2013@163.com>
2013-03-09 01:15:40 +0800

    log4j

--- src/com/wendongwei/repository/test/Test.java
+++ src/com/wendongwei/repository/test/Test.java
@@ -1,10 +1,12 @@
 package com.wendongwei.repository.test;
 
+import org.apache.log4j.Logger;
+
 public class Test
 {
+	private static Logger logger  =  Logger.getLogger(Test.class ); 
 	public static void main(String[] args)
 	{
-		System.out.print("Hello github");
-		System.out.print("Hello github");
+		logger.info("Hello github");
 	}
 }
