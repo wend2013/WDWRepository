dbb16a1de18a42811e84c4fd94317c58fcebbe02
wendongwei <wendongwei@220.250.64.225>
2013-04-11 22:34:00 +0800

    wdw

---
+++ src/com/wendongwei/repository/jdbc/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright wend2013 All Rights Reserved.
+ */
+/**
+ * @ClassName: jdbc示例
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author wend2013 Email： wend2013@163.com
+ * @date 2013-3-24 - 下午10:45:38
+ *@version : 1.0
+ */
+package com.wendongwei.repository.jdbc;
